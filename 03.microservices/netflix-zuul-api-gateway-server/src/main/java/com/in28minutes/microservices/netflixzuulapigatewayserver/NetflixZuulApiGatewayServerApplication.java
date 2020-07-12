95ccd259cd78977215716219e348c43ff0e482e5
Kedir Birhan <kedirbirhan@yahoo.com>
2020-07-12 06:29:56 -0400

    using http://localhost:8765/currency-exchange-service/currency-exchange/from/USD/to/INR url to execute the currecy exchnage service through zuul api gateway proxy

--- 03.microservices/netflix-zuul-api-gateway-server/src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ 03.microservices/netflix-zuul-api-gateway-server/src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
-import brave.sampler.Sampler;
+//import brave.sampler.Sampler;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -17,8 +17,8 @@ public class NetflixZuulApiGatewayServerApplication {
 		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
 	}
 	
-	@Bean
-	public Sampler defaultSampler(){
-		return Sampler.ALWAYS_SAMPLE;
-	}
+//	@Bean
+//	public Sampler defaultSampler(){
+//		return Sampler.ALWAYS_SAMPLE;
+//	}
 }
